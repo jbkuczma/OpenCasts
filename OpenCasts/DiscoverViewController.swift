fa899962d9680d4f8b64276b2079e04eb6459a41
James <jbkuczma@buffalo.edu>
2017-08-27 09:30:54 -0400

    added search bar and uitableview now displays all rows

--- OpenCasts/DiscoverViewController.swift
+++ OpenCasts/DiscoverViewController.swift
@@ -11,20 +11,55 @@ import UIKit
 class DiscoverViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     private let categories: NSArray = ["All", "Arts", "Buisness", "Comedy", "Education", "Games & Hobbies", "Government", "Health", "Family", "Religious", "Science & Medicine", "Social", "Sports", "Technology", "TV & Film"]
+    
+    private var categoryTableLabel: UILabel!
     private var categoryTableView: UITableView!
+    private var searchInput: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        createSearchInput()
+        createCategoryTable()
+        
+        self.categoryTableView.allowsSelection = true
+    }
+    
+    func createSearchInput() {
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        
+        searchInput = UISearchBar(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: 50))
+        searchInput.placeholder = "Search"
+        self.view.addSubview(searchInput) // add search input to window
+    }
+    
+    func createCategoryTable() {
+        let tabBarHeight = self.tabBarController?.tabBar.bounds.height
         let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
         let displayWidth: CGFloat = self.view.frame.width
         let displayHeight: CGFloat = self.view.frame.height
         
-        categoryTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        /* Create label for table */
+        categoryTableLabel = UILabel(frame: CGRect(x: 0, y: barHeight + 50, width: displayWidth, height: 50))
+        categoryTableLabel.text = "Categories"
+        categoryTableLabel.textColor = UIColor(
+            red: CGFloat(244/255.0),
+            green: CGFloat(67/255.0),
+            blue: CGFloat(54/255.0),
+            alpha: CGFloat(1.0)
+        )
+        categoryTableLabel.textAlignment = .center
+        self.view.addSubview(categoryTableLabel)
+        /**************************/
+        /* Create table */
+        // height: displayHeight (height of window) - barHeight (height of statusBar) - tabBarHeight(height of tab nav bar at bottom) - 100 (two rows were being cut off)
+        categoryTableView = UITableView(frame: CGRect(x: 0, y: barHeight + 100, width: displayWidth, height: displayHeight - barHeight - tabBarHeight! - 100))
         categoryTableView.register(UITableViewCell.self, forCellReuseIdentifier: "CategoryCell")
         categoryTableView.dataSource = self
         categoryTableView.delegate = self
-        self.view.addSubview(categoryTableView)
+        self.view.addSubview(categoryTableView) // add table to window
+        /**************************/
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
