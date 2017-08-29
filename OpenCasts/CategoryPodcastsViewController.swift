9e3114e807c803ba904873df3d2f8f90ba7cff12
James <jbkuczma@buffalo.edu>
2017-08-29 16:05:36 -0400

    fetch data and use the returned json

--- OpenCasts/CategoryPodcastsViewController.swift
+++ OpenCasts/CategoryPodcastsViewController.swift
@@ -11,12 +11,18 @@ import UIKit
 class CategoryPodcastsViewController: UIViewController {
     
     var category: String!
+    var count: Int!
+    var episodes = [[String: Any]]() // array of dictionaries
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print(category)
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.getData()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -27,5 +33,15 @@ class CategoryPodcastsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // fetch data
+    private func getData() {
+        let r = Request()
+        r.search(query: category, completion: {(data) in
+            self.count = data?["resultCount"] as! Int
+            self.episodes = data?["results"] as! [[String: Any]]
+            print(self.count)
+        })
     }
 }
