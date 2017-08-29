62fd4b1d24461a30e62dd70dd74f00d01d58979a
James <jbkuczma@buffalo.edu>
2017-08-29 11:34:55 -0400

    class that will make search api requests. currently uses the itunes seach api

--- OpenCasts/Request.swift
+++ OpenCasts/Request.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+class Request {
+    
+    private let baseURL: String = "https://itunes.apple.com/search"
+    
+    init() {
+        
+    }
+    
+    func createSearchURL(query: String) -> URL {
+        let queryItems = [
+            NSURLQueryItem(name: "entity", value: "podcast"),
+            NSURLQueryItem(name: "term", value: query),
+            NSURLQueryItem(name: "limit", value: "100")
+        ]
+        let urlComps = NSURLComponents(string: baseURL)!
+        urlComps.queryItems = queryItems as [URLQueryItem]
+        let searchURL = urlComps.url!
+        return searchURL
+    }
+    
+    func search(query: String) {
+        let url: URL = self.createSearchURL(query: query)
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: [])
+            print(json)
+        }
+        task.resume()
+    }
+}
